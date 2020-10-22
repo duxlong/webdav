50359fff84b269501094d9ec3d126e5e90b25ebf
duxlong <duxlong@me.com>
2020-10-22 21:58:14 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,10 @@ if [ -e /opt/nginx/conf/htpasswd ]; then exit 0; fi
 if [ -n "$USERNAME" ]
 then
   htpasswd -bc /opt/nginx/conf/htpasswd $USERNAME $PASSWORD
-  echo "generate htpasswd"
+  echo "成功添加单用户！"
   exit 0
 fi
 
 # 两种用户模式都没有设置，则报错提示
-echo "检查是否设置用户"
+echo "错误！检查是否正确配置用户！"
 exit 1
